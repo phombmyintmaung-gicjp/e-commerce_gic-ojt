8b44dbf9804edd3cd4edb0cd120d2471bfd3b1ad
phombmyintmaunggicjp <phombmyintmaung@gicjp.com>
2025-08-01 15:49:28 +0630

    add pagination component 08012025

--- frontend/src/screens/Admin/orderlist/OrdersList.jsx
+++ frontend/src/screens/Admin/orderlist/OrdersList.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import EditIcon from "../../../assets/edit_icon.svg";
 import DeleteIcon from "../../../assets/delete_icon.svg";
+import Pagination from "../../../components/Pagination";
 const mockOrders = [
   {
     orderId: 1,
@@ -170,34 +171,12 @@ const OrdersList = () => {
           </tbody>
         </table>
       </div>
-
       {/* Pagination */}
-      <div className="flex justify-center mt-8 gap-2 text-[14px]">
-        <button
-          onClick={() => goToPage(currentPage - 1)}
-          disabled={currentPage === 1}
-          className="px-2">
-          {"<"}
-        </button>
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map((num) => (
-          <button
-            key={num}
-            onClick={() => goToPage(num)}
-            className={`px-3 py-1 rounded ${
-              num === currentPage
-                ? "bg-[var(--color-highlight)] text-[var(--color-black)]"
-                : "text-[var(--color-black)]"
-            }`}>
-            {num}
-          </button>
-        ))}
-        <button
-          onClick={() => goToPage(currentPage + 1)}
-          disabled={currentPage === totalPages}
-          className="px-2">
-          {">"}
-        </button>
-      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
     </section>
   );
 };
